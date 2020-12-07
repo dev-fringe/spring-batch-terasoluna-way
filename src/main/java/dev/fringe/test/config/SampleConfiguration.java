66ec54b98918464d3525a39d63f5ff0d0e0da86e
Lee Hak Do <dev.fringe@gmail.com>
2020-12-07 19:01:59 +0900

    modi

--- src/main/java/dev/fringe/test/config/SampleConfiguration.java
+++ src/main/java/dev/fringe/test/config/SampleConfiguration.java
@@ -1,7 +1,6 @@
 package dev.fringe.test.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.scope.StepScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
@@ -15,8 +14,4 @@ public class SampleConfiguration {
 	    return new SimpleAsyncTaskExecutor();
 	}
 	
-	@Bean
-	public StepScope stepScope() {
-	    return new StepScope();
-	}
 }
